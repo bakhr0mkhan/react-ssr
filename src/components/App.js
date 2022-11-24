1bb7a12c8f6d2629f8abe0a2c39e9518abce3730
bakhr0mkhan <softdeveloperuz@gmail.com>
2022-11-24 14:29:50 +0500

    Initial

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "../styles/styles.scss"
+
+export default function App() {
+    return (
+        <div>
+            <h1>React webpack file</h1>
+        </div>
+    )
+}
